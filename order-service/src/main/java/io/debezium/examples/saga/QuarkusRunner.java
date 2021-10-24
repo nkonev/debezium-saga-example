0bb176d5db9cc1d3920769c06dd1c859ef123beb
Nikita Konev <nikit.cpp@yandex.ru>
2021-10-24 12:24:29 +0300

    initial commit, from debezium-examples/saga

---
+++ order-service/src/main/java/io/debezium/examples/saga/QuarkusRunner.java
@@ -0,0 +1,11 @@
+//package io.debezium.examples.saga;
+//import io.quarkus.runtime.annotations.QuarkusMain;
+//import io.quarkus.runtime.Quarkus;
+//
+//@QuarkusMain
+//public class QuarkusRunner {
+//
+//    public static void main(String ... args) {
+//        Quarkus.run(args);
+//    }
+//}
